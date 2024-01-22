235bb5bd0f6ed1cb66334af47f7660a5e384c840
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-01-22 20:43:16 +0700

    Initial commit

---
+++ src/main/java/com/bayu/csvfileservice/CsvFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.csvfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvFileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/csvfileservice/CsvFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.csvfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
