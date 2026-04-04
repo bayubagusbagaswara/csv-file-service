dbbce05568a5b5f5db3e29cb7eab207886921bd3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-04 16:42:00 +0700

    add enumerator FeatureType

---
+++ src/main/java/com/bayu/csvfileservice/model/enumerator/FeatureType.java
@@ -0,0 +1,13 @@
+package com.bayu.csvfileservice.model.enumerator;
+
+import lombok.Getter;
+
+@Getter
+public enum FeatureType {
+
+    DEPOSIT_TRANSFER,
+    MANAGEMENT_FEE,
+    TAX_PAYMENT,
+    STOCK_PURCHASE
+
+}
