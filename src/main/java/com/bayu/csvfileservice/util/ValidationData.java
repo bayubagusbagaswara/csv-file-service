1137361fe0e8c74a60a31b1a091aa4593e941ffd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 15:50:37 +0700

    add ValidationData

---
+++ src/main/java/com/bayu/csvfileservice/util/ValidationData.java
@@ -0,0 +1,22 @@
+package com.bayu.csvfileservice.util;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class ValidationData {
+
+    private final Validator validator;
+
+    public ValidationData(Validator validator) {
+        this.validator = validator;
+    }
+
+    public <T> Set<ConstraintViolation<T>> validateObject(T target, Class<?>... groups) {
+        return validator.validate(target, groups);
+    }
+
+}
