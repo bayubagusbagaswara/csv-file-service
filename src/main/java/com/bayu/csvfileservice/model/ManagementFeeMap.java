74ffc89e0f133983944a260feaa63c296b4e5dbc
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 19:25:12 +0700

    add entity ManagementFeeMap

---
+++ src/main/java/com/bayu/csvfileservice/model/ManagementFeeMap.java
@@ -0,0 +1,63 @@
+package com.bayu.csvfileservice.model;
+
+import com.bayu.csvfileservice.model.base.BaseApproval;
+import com.bayu.csvfileservice.model.enumerator.MappingStatus;
+import com.bayu.csvfileservice.model.enumerator.Month;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "management_fee_map")
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ManagementFeeMap extends BaseApproval {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Month month;
+
+    private Integer year;
+
+    private String mutualFundName;
+
+    private String investmentManager;
+
+    private String fundCode;
+
+    private String debitAccount;
+
+    private BigDecimal amount;
+
+    private String creditAccount;
+
+    private String beneficiaryName;
+
+    private String bankName;
+
+    private String paymentInstructions;
+
+    private String paymentType;
+
+    private String period;
+
+    private String description;
+
+    private String bankCode;
+
+    @Enumerated(EnumType.STRING)
+    private MappingStatus status;
+
+    private Long ncbsRequestId; // relasi ke request
+
+}
