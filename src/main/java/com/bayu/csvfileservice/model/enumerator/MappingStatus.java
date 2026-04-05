cf7d281ba75e5611e8e890caab55304fa308343f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 19:26:07 +0700

    add enumerator MappingStatus

---
+++ src/main/java/com/bayu/csvfileservice/model/enumerator/MappingStatus.java
@@ -0,0 +1,13 @@
+package com.bayu.csvfileservice.model.enumerator;
+
+import lombok.Getter;
+
+@Getter
+public enum MappingStatus {
+    DRAFT,
+    READY,
+    SENT,
+    SUCCESS,
+    FAILED,
+    RETRY
+}
