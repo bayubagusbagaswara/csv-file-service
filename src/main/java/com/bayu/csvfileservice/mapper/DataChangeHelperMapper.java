0f7360bfaed966e3bdeb56c2e4c62f291be2ed65
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 15:50:17 +0700

    add DataChangeHelperMapper

---
+++ src/main/java/com/bayu/csvfileservice/mapper/DataChangeHelperMapper.java
@@ -0,0 +1,53 @@
+package com.bayu.csvfileservice.mapper;
+
+import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
+import com.bayu.csvfileservice.exception.JsonSerializeException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+@RequiredArgsConstructor
+public abstract class DataChangeHelperMapper {
+
+    protected ObjectMapper objectMapper;
+
+    public <T> DataChangeDto toAuditDto(
+            DataChangeDto baseDto,
+            T before,
+            T after
+    ) {
+        try {
+            String jsonBefore = before != null
+                    ? objectMapper.writeValueAsString(before)
+                    : null;
+
+            String jsonAfter = after != null
+                    ? objectMapper.writeValueAsString(after)
+                    : null;
+
+            return baseDto.toBuilder()
+                    .jsonDataBefore(jsonBefore)
+                    .jsonDataAfter(jsonAfter)
+                    .build();
+
+        } catch (Exception e) {
+            throw new JsonSerializeException(
+                    "Failed to serialize payload: " + e.getMessage(), e
+            );
+        }
+    }
+
+    public <T> DataChangeDto forAdd(DataChangeDto baseDto, T after) {
+        return toAuditDto(baseDto, null, after);
+    }
+
+    public <T> DataChangeDto forEdit(DataChangeDto baseDto, T before, T after) {
+        return toAuditDto(baseDto, before, after);
+    }
+
+    public <T> DataChangeDto forDelete(DataChangeDto baseDto, T before) {
+        return toAuditDto(baseDto, before, null);
+    }
+
+}
