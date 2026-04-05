fdc1de8fb8ccc4809f0aab7c7ab34d3762712dc6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 13:47:08 +0700

    add service ManagementFeeRawService

---
+++ src/main/java/com/bayu/csvfileservice/service/ManagementFeeRawService.java
@@ -0,0 +1,11 @@
+package com.bayu.csvfileservice.service;
+
+import com.bayu.csvfileservice.dto.ProcessResult;
+import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
+import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
+
+public interface ManagementFeeRawService {
+
+    ProcessResult createBulk(ManagementFeeBulkRequest request, DataChangeDto dataChangeDto);
+
+}
