ff7106781fef644c9fd80a42ac3500c45ae29b5a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 13:46:44 +0700

    add entity ManagementFeeRaw

---
+++ src/main/java/com/bayu/csvfileservice/model/ManagementFeeRaw.java
@@ -0,0 +1,55 @@
+package com.bayu.csvfileservice.model;
+
+import com.bayu.csvfileservice.model.base.BaseApproval;
+import com.bayu.csvfileservice.model.enumerator.Month;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "management_fee_raw")
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ManagementFeeRaw extends BaseApproval {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Month month;
+
+    private Integer year;
+
+    private String mutualFundName;
+
+    private String investmentManager;
+
+    private String debitAccount;
+
+    private BigDecimal amount;
+
+    private String creditAccount; // rekening tujuan
+
+    private String beneficiaryName; // atas nama
+
+    private String bankName;
+
+    private String paymentInstructions;
+
+    private String paymentType;
+
+    private String period;
+
+    private String description;
+
+    private String bankCode;
+
+}
