39d838cc0290f3a4233758e0fc7749ce33986aac
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 13:45:09 +0700

    add service DataChangeService

---
+++ src/main/java/com/bayu/csvfileservice/service/DataChangeService.java
@@ -0,0 +1,21 @@
+package com.bayu.csvfileservice.service;
+
+import com.bayu.csvfileservice.model.DataChange;
+
+import java.util.List;
+
+public interface DataChangeService {
+
+    DataChange getById(Long id);
+
+    void setApprovalStatusIsRejected(DataChange dataChange, List<String> messages);
+
+    void setApprovalStatusIsApproved(DataChange dataChange);
+
+    <T> void createChangeActionAdd(DataChange dataChange, Class<T> clazz);
+
+    <T> void createChangeActionEdit(DataChange dataChange, Class<T> clazz);
+
+    <T> void createChangeActionDelete(DataChange dataChange, Class<T> clazz);
+
+}
