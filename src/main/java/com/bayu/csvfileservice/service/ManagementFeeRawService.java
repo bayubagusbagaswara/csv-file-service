da8e974f6859114bb43ff046c4a1af8795dec64d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 15:51:19 +0700

    add method createApprove

--- src/main/java/com/bayu/csvfileservice/service/ManagementFeeRawService.java
+++ src/main/java/com/bayu/csvfileservice/service/ManagementFeeRawService.java
@@ -1,5 +1,6 @@
 package com.bayu.csvfileservice.service;
 
+import com.bayu.csvfileservice.dto.ApproveDataChangeRequest;
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
@@ -7,5 +8,7 @@ import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
 public interface ManagementFeeRawService {
 
     ProcessResult createBulk(ManagementFeeBulkRequest request, DataChangeDto dataChangeDto);
+
+    ProcessResult createApprove(ApproveDataChangeRequest request, String clientIp);
 
 }
