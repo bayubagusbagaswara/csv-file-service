2942bc93b88094e73eefbbadc02b0b87750b3d61
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 13:47:40 +0700

    add enumerator MiddlewareServiceType

---
+++ src/main/java/com/bayu/csvfileservice/model/enumerator/MiddlewareServiceType.java
@@ -0,0 +1,12 @@
+package com.bayu.csvfileservice.model.enumerator;
+
+import lombok.Getter;
+
+@Getter
+public enum MiddlewareServiceType {
+
+    INQUIRY_ACCOUNT,
+    CREDIT_TRANSFER,
+    OVERBOOKING_CASA,
+    TRANSFER_SKN_RTGS
+}
