3ecbd951a960f0ee2304f395b07d908b1d63f9c9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 18:41:57 +0700

    add method toDto

--- src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
@@ -1,12 +1,16 @@
 package com.bayu.csvfileservice.mapper;
 
+import com.bayu.csvfileservice.dto.managementfee.ManagementFeeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeRequest;
 import com.bayu.csvfileservice.dto.managementfee.ViewManagementFeeAfterAdd;
+import com.bayu.csvfileservice.model.ManagementFeeRaw;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ManagementFeeMapper {
 
     ViewManagementFeeAfterAdd fromRequestToViewAfterAdd(ManagementFeeRequest request);
+
+    ManagementFeeDto toDto(ManagementFeeRaw managementFeeRaw);
 
 }
