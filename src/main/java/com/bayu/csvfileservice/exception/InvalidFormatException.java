fd71d41ec915ff53d72f84b65e18f3eb908c8cba
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-05 18:41:26 +0700

    add exception InvalidFormatException

---
+++ src/main/java/com/bayu/csvfileservice/exception/InvalidFormatException.java
@@ -0,0 +1,15 @@
+package com.bayu.csvfileservice.exception;
+
+public class InvalidFormatException extends RuntimeException {
+
+    public InvalidFormatException() {
+    }
+
+    public InvalidFormatException(String message) {
+        super(message);
+    }
+
+    public InvalidFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
