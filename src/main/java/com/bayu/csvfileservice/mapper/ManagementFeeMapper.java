66453541dd2d3646fb95bd74187121f3c1bfab53
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-07 07:16:31 +0700

    add method fromRequestToViewBeforeDelete

--- src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
@@ -3,6 +3,7 @@ package com.bayu.csvfileservice.mapper;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeRequest;
 import com.bayu.csvfileservice.dto.managementfee.ViewManagementFeeAfterAdd;
+import com.bayu.csvfileservice.dto.managementfee.ViewManagementFeeBeforeDelete;
 import com.bayu.csvfileservice.model.ManagementFeeRaw;
 import org.mapstruct.Mapper;
 
@@ -12,5 +13,7 @@ public interface ManagementFeeMapper {
     ViewManagementFeeAfterAdd fromRequestToViewAfterAdd(ManagementFeeRequest request);
 
     ManagementFeeDto toDto(ManagementFeeRaw managementFeeRaw);
+
+    ViewManagementFeeBeforeDelete fromRequestToViewBeforeDelete(ManagementFeeDto dto);
 
 }
