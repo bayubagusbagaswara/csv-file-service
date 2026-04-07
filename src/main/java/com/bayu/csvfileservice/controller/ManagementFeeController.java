12f6be50a3fefc4b8c6f6e40e4eec354a16a421f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-07 07:16:00 +0700

    update ManagementFeeController

--- src/main/java/com/bayu/csvfileservice/controller/ManagementFeeController.java
+++ src/main/java/com/bayu/csvfileservice/controller/ManagementFeeController.java
@@ -5,8 +5,6 @@ import com.bayu.csvfileservice.dto.ApproveDataChangeRequest;
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
-import com.bayu.csvfileservice.exception.InvalidFormatException;
-import com.bayu.csvfileservice.model.enumerator.Month;
 import com.bayu.csvfileservice.service.ManagementFeeMapService;
 import com.bayu.csvfileservice.service.ManagementFeeRawService;
 import com.bayu.csvfileservice.util.ClientIpUtil;
@@ -16,9 +14,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/management-fee")
@@ -33,7 +32,7 @@ public class ManagementFeeController {
     private final ManagementFeeMapService mapService;
     private final DataChangeFactory dataChangeFactory;
 
-    @PostMapping(path = "/upload-raw")
+    @PostMapping(path = "/upload-bulk")
     public ResponseEntity<ApiResponse<ProcessResult>> createBulk(@RequestBody ManagementFeeBulkRequest request, HttpServletRequest servletRequest) {
         String clientIp = ClientIpUtil.getClientIp(servletRequest);
         DataChangeDto dataChangeDto = dataChangeFactory.build(
@@ -54,44 +53,6 @@ public class ManagementFeeController {
         return buildResponse(result);
     }
 
-    @PostMapping(path = "/map")
-    public ResponseEntity<ApiResponse<ProcessResult>> map(
-            @RequestParam("month") String month,
-            @RequestParam("year") Integer year,
-            HttpServletRequest servletRequest
-    ) {
-        Month monthEnum = parseMonth(month);
-        ProcessResult result = mapService.map(monthEnum, year);
-        return buildResponse(result);
-    }
-
-
-    @PostMapping(path = "/map/approve")
-    public ResponseEntity<ApiResponse<ProcessResult>> mapApprove(
-            @RequestBody List<Long> idList,
-            HttpServletRequest servletRequest
-    ) {
-        // endpoint /map/approve untuk trigger terbang ke Middleware
-        // Saat user melakukan approve maka akan terkirim ke MiddlewareService
-        // Lalu dari MiddlewareService akan mengembalikan responseStatus dan responseCode
-        // Jika responseStatus adalah Success, maka data di table ManagementRawMap tidak akan bisa dikirim ulang (atau disable)
-        // Jika responseStatus adalah Saldo Kurang, maka data di table ManagementRawMap akan enable lagi
-        ProcessResult result = mapService.approve(idList);
-        return buildResponse(result);
-    }
-
-    @PostMapping(path = "/map/send")
-    public ResponseEntity<ApiResponse<ProcessResult>> send(
-            @RequestBody List<Long> idList,
-            HttpServletRequest servletRequest
-    ) {
-        ProcessResult result = mapService.sendToMiddleware(idList);
-        return buildResponse(result);
-    }
-
-
-
-
     private <T> ResponseEntity<ApiResponse<T>> buildResponse(T payload) {
         ApiResponse<T> response = ApiResponse.<T>builder()
                 .code(200)
@@ -100,14 +61,6 @@ public class ManagementFeeController {
                 .build();
 
         return ResponseEntity.ok(response);
-    }
-
-    private Month parseMonth(String month) {
-        try {
-            return Month.valueOf(month.toUpperCase());
-        } catch (Exception e) {
-            throw new InvalidFormatException("Invalid month: " + month);
-        }
     }
 
 }
