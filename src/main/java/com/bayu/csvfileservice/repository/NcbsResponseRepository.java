34da5f7c9cc14f890201f9c011fe0c4baa9106e7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-07 07:17:39 +0700

    add repository NcbsResponseRepository

---
+++ src/main/java/com/bayu/csvfileservice/repository/NcbsResponseRepository.java
@@ -0,0 +1,7 @@
+package com.bayu.csvfileservice.repository;
+
+import com.bayu.csvfileservice.model.NcbsResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NcbsResponseRepository extends JpaRepository<NcbsResponse, Long> {
+}
