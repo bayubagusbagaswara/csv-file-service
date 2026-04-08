105f582da9ca09faa65569c749614c4f67cab5bc
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:41:06 +0700

    add method getAll

--- src/main/java/com/bayu/csvfileservice/service/ManagementFeeRawService.java
+++ src/main/java/com/bayu/csvfileservice/service/ManagementFeeRawService.java
@@ -4,6 +4,9 @@ import com.bayu.csvfileservice.dto.ApproveDataChangeRequest;
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
+import com.bayu.csvfileservice.model.ManagementFeeRaw;
+
+import java.util.List;
 
 public interface ManagementFeeRawService {
 
@@ -11,4 +14,5 @@ public interface ManagementFeeRawService {
 
     ProcessResult createApprove(ApproveDataChangeRequest request, String clientIp);
 
+    List<ManagementFeeRaw> getAll();
 }
