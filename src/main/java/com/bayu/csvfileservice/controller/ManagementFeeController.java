bb23000d522292b85cabfd034d637d7eb80e484d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 06:23:34 +0700

    add deleteById and deleteApprove

--- src/main/java/com/bayu/csvfileservice/controller/ManagementFeeController.java
+++ src/main/java/com/bayu/csvfileservice/controller/ManagementFeeController.java
@@ -2,9 +2,11 @@ package com.bayu.csvfileservice.controller;
 
 import com.bayu.csvfileservice.dto.ApiResponse;
 import com.bayu.csvfileservice.dto.ApproveDataChangeRequest;
+import com.bayu.csvfileservice.dto.DeleteIdRequest;
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.datachange.DataChangeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeBulkRequest;
+import com.bayu.csvfileservice.model.ManagementFeeRaw;
 import com.bayu.csvfileservice.service.ManagementFeeMapService;
 import com.bayu.csvfileservice.service.ManagementFeeRawService;
 import com.bayu.csvfileservice.util.ClientIpUtil;
@@ -14,10 +16,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/management-fee")
@@ -50,6 +51,39 @@ public class ManagementFeeController {
     public ResponseEntity<ApiResponse<ProcessResult>> createApprove(@RequestBody ApproveDataChangeRequest request, HttpServletRequest servletRequest) {
         String clientIp = ClientIpUtil.getClientIp(servletRequest);
         ProcessResult result = rawService.createApprove(request, clientIp);
+        return buildResponse(result);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<ApiResponse<List<ManagementFeeRaw>>> getAll() {
+        List<ManagementFeeRaw> list = rawService.getAll();
+        return buildResponse(list);
+    }
+
+    @DeleteMapping(path = "/deleteById")
+    public ResponseEntity<ApiResponse<ProcessResult>> deleteById(
+            @RequestBody DeleteIdRequest request,
+            HttpServletRequest servletRequest
+    ) {
+        String clientIp = ClientIpUtil.getClientIp(servletRequest);
+        DataChangeDto dataChangeDto = dataChangeFactory.build(
+                request.getInputId(),
+                clientIp,
+                HttpMethod.DELETE.name(),
+                BASE_URL_MANAGEMENT_FEE + "/delete/approve",
+                MENU_MANAGEMENT_FEE
+        );
+        ProcessResult result = rawService.deleteById(request, dataChangeDto);
+        return buildResponse(result);
+    }
+
+    @DeleteMapping(path = "/delete/approve")
+    public ResponseEntity<ApiResponse<ProcessResult>> deleteApprove(
+            @RequestBody ApproveDataChangeRequest request,
+            HttpServletRequest servletRequest
+    ) {
+        String clientIp = ClientIpUtil.getClientIp(servletRequest);
+        ProcessResult result = rawService.deleteApprove(request, clientIp);
         return buildResponse(result);
     }
 
