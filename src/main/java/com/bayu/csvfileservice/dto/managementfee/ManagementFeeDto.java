4236072e8c6c20bc62d4f64b23583325354a0739
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:40:13 +0700

    add JsonView

--- src/main/java/com/bayu/csvfileservice/dto/managementfee/ManagementFeeDto.java
+++ src/main/java/com/bayu/csvfileservice/dto/managementfee/ManagementFeeDto.java
@@ -1,5 +1,8 @@
 package com.bayu.csvfileservice.dto.managementfee;
 
+import com.bayu.csvfileservice.util.JsonViews;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,22 +14,55 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ManagementFeeDto {
 
+    @JsonView(JsonViews.Query.class)  // Hanya tampil di Query
     private Long id;
+
+    @JsonView(JsonViews.Query.class)  // Hanya tampil di Query
     private String month;
+
+    @JsonView(JsonViews.Query.class)  // Hanya tampil di Query
     private Integer year;
 
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})  // Tampil di kedua view
+    @JsonProperty("namaReksadana")
     private String mutualFundName;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String investmentManager;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String fundCode;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String debitAccount;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String amount;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String creditAccount;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String beneficiaryName;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String bankName;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String paymentInstructions;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String paymentType;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String period;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String description;
+
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
     private String bankCode;
 
+    @JsonView({JsonViews.Audit.class, JsonViews.Query.class})
+    private String referenceCombination;
 }
