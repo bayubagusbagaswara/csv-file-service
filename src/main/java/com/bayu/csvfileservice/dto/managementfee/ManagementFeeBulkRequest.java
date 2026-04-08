e7c143a379def5ab99eef070c38c39ad08a93fb8
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:40:00 +0700

    update ManagementFeeBulkRequest

--- src/main/java/com/bayu/csvfileservice/dto/managementfee/ManagementFeeBulkRequest.java
+++ src/main/java/com/bayu/csvfileservice/dto/managementfee/ManagementFeeBulkRequest.java
@@ -1,15 +1,12 @@
 package com.bayu.csvfileservice.dto.managementfee;
 
 import com.bayu.csvfileservice.dto.InputIdentifierRequest;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.SuperBuilder;
+import lombok.*;
 
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@SuperBuilder
 public class ManagementFeeBulkRequest extends InputIdentifierRequest {
 
     private List<ManagementFeeRequest> items;
