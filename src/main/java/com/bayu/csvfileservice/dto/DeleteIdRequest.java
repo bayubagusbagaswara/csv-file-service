9cff0b245d2697e205d968483fb88d34e0924a32
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:39:32 +0700

    update DeleteIdRequest

--- src/main/java/com/bayu/csvfileservice/dto/DeleteIdRequest.java
+++ src/main/java/com/bayu/csvfileservice/dto/DeleteIdRequest.java
@@ -2,12 +2,10 @@ package com.bayu.csvfileservice.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@SuperBuilder
-public class DeleteIdRequest extends InputIdentifierRequest{
+public class DeleteIdRequest extends InputIdentifierRequest {
 
     private Long id;
 
