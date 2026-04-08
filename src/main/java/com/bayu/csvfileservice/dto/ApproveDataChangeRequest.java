8a148acac9fbe5f8289d476f1041799db36fad3e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:38:48 +0700

    update ApproveDataChangeRequest

--- src/main/java/com/bayu/csvfileservice/dto/ApproveDataChangeRequest.java
+++ src/main/java/com/bayu/csvfileservice/dto/ApproveDataChangeRequest.java
@@ -2,11 +2,9 @@ package com.bayu.csvfileservice.dto;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
-@SuperBuilder
 public class ApproveDataChangeRequest extends ApprovalIdentifierRequest {
 
     private Long dataChangeId;
