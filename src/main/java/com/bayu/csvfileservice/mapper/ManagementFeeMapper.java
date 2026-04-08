21285ea5fd67437a549146ee67834a48c71f9824
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:40:26 +0700

    update ManagementFeeMapper

--- src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/ManagementFeeMapper.java
@@ -2,18 +2,28 @@ package com.bayu.csvfileservice.mapper;
 
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeDto;
 import com.bayu.csvfileservice.dto.managementfee.ManagementFeeRequest;
-import com.bayu.csvfileservice.dto.managementfee.ViewManagementFeeAfterAdd;
-import com.bayu.csvfileservice.dto.managementfee.ViewManagementFeeBeforeDelete;
 import com.bayu.csvfileservice.model.ManagementFeeRaw;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ManagementFeeMapper {
 
-    ViewManagementFeeAfterAdd fromRequestToViewAfterAdd(ManagementFeeRequest request);
+    // tambahkan field referenceCombine
+    @Mapping(
+            target = "referenceCombination",
+            expression = "java(createReferenceCombination(request.getFundCode(), request.getAmount()))"
+    )
+    ManagementFeeDto fromRequestToDto(ManagementFeeRequest request);
 
-    ManagementFeeDto toDto(ManagementFeeRaw managementFeeRaw);
+    ManagementFeeDto toDto(ManagementFeeRaw entity);
 
-    ViewManagementFeeBeforeDelete fromRequestToViewBeforeDelete(ManagementFeeDto dto);
+    ManagementFeeDto fromEntityToDto(ManagementFeeRaw entity);
 
+    default String createReferenceCombination(String fundCode, String amount) {
+        if (fundCode == null || amount == null) {
+            return null;
+        }
+        return fundCode + amount.replace(",", "").replace(".", "");
+    }
 }
