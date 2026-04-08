96b1b135d6913e49c266a1d44e225f76008b9afd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-08 22:01:16 +0700

    add JsonViews

---
+++ src/main/java/com/bayu/csvfileservice/util/JsonViews.java
@@ -0,0 +1,6 @@
+package com.bayu.csvfileservice.util;
+
+public class JsonViews {
+    public static class Audit {}      // Untuk audit (exclude id, month, year)
+    public static class Query {}      // Untuk query (include semua field)
+}
