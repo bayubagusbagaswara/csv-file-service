746e15787c79924ff127176ed09cf24da584395d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-09 05:39:43 +0700

    update InputIdentifierRequest

--- src/main/java/com/bayu/csvfileservice/dto/InputIdentifierRequest.java
+++ src/main/java/com/bayu/csvfileservice/dto/InputIdentifierRequest.java
@@ -1,10 +1,8 @@
 package com.bayu.csvfileservice.dto;
 
 import lombok.Data;
-import lombok.experimental.SuperBuilder;
 
 @Data
-@SuperBuilder
 public abstract class InputIdentifierRequest {
 
     private String inputId;
