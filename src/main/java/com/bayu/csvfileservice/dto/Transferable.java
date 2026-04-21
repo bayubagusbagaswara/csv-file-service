5f8ff15744457a67c7932b9b8692345ec5fd8fb6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-21 19:43:27 +0700

    add Transferable

---
+++ src/main/java/com/bayu/csvfileservice/dto/Transferable.java
@@ -0,0 +1,30 @@
+package com.bayu.csvfileservice.dto;
+
+import com.bayu.csvfileservice.model.enumerator.FeatureType;
+import com.bayu.csvfileservice.model.enumerator.ProcessType;
+import com.bayu.csvfileservice.model.enumerator.TransferMethod;
+import com.bayu.csvfileservice.model.enumerator.TransferScope;
+
+import java.math.BigDecimal;
+
+public interface Transferable {
+
+    Long getId();
+
+    String getDebitAccount();
+
+    String getCreditAccount();
+
+    BigDecimal getAmount();
+
+    String getDescription();
+
+    TransferMethod getTransferMethod();
+
+    TransferScope getTransferScope();
+
+    ProcessType getProcessType();
+
+    FeatureType getFeatureType();
+
+}
