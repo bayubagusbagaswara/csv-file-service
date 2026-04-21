e42dfba37ea503e6f0c7ae9a05e8205c74e55f0a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-21 19:33:24 +0700

    update enum ProcessType

--- src/main/java/com/bayu/csvfileservice/model/enumerator/ProcessType.java
+++ src/main/java/com/bayu/csvfileservice/model/enumerator/ProcessType.java
@@ -5,7 +5,23 @@ import lombok.Getter;
 @Getter
 public enum ProcessType {
 
-    SINGLE,
-    BULK
+    SINGLE("Single"),
+    BULK("Bulk")
+    ;
+
+    private final String name;
+
+    ProcessType(String name) {
+        this.name = name;
+    }
+
+    public static ProcessType fromName(String name) {
+        for (ProcessType value : values()) {
+            if (value.name.equalsIgnoreCase(name)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid process type: " + name);
+    }
 
 }
