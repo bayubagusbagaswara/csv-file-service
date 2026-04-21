8ae9c6713913176ffb8480f8309ea49b259336be
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-21 20:30:25 +0700

    add methods repository

--- src/main/java/com/bayu/csvfileservice/repository/ManagementFeeMapRepository.java
+++ src/main/java/com/bayu/csvfileservice/repository/ManagementFeeMapRepository.java
@@ -1,11 +1,16 @@
 package com.bayu.csvfileservice.repository;
 
 import com.bayu.csvfileservice.model.ManagementFeeMap;
+import com.bayu.csvfileservice.model.enumerator.MappingStatus;
 import com.bayu.csvfileservice.model.enumerator.Month;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ManagementFeeMapRepository extends JpaRepository<ManagementFeeMap, Long> {
 
     void deleteByMonthAndYear(Month month, Integer year);
+
+    void deleteByMonthAdnYearAndStatus(Month month, Integer year, MappingStatus status);
+
+    boolean existsByReferenceCombination(String referenceCombination);
 
 }
