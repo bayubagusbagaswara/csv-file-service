c32962f6b72a65d1e76b9912aa1427cadd577e26
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-21 19:31:27 +0700

    update enum MiddlewareServiceType

--- src/main/java/com/bayu/csvfileservice/model/enumerator/MiddlewareServiceType.java
+++ src/main/java/com/bayu/csvfileservice/model/enumerator/MiddlewareServiceType.java
@@ -5,8 +5,26 @@ import lombok.Getter;
 @Getter
 public enum MiddlewareServiceType {
 
-    INQUIRY_ACCOUNT,
-    CREDIT_TRANSFER,
-    OVERBOOKING_CASA,
-    TRANSFER_SKN_RTGS
+    INQUIRY_ACCOUNT("INQUIRY_ACCOUNT"),
+    CREDIT_TRANSFER("CREDIT_TRANSFER"),
+    OVERBOOKING_CASA_TO_CASA("OVERBOOKING_CASA_TO_CASA"),
+    TRANSFER_SKN_RTGS("TRANSFER_SKN_RTGS"),
+    BI_FAST_PAYMENT_STATUS("BI_FAST_PAYMENT_STATUS"),
+    OVERBOOKING_CASA_TO_GL("OVERBOOKING_CASA_TO_GL");
+
+    private final String name;
+
+    MiddlewareServiceType(String name) {
+        this.name = name;
+    }
+
+    public static MiddlewareServiceType fromName(String name) {
+        for (MiddlewareServiceType value : values()) {
+            if (value.name.equalsIgnoreCase(name)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid middleware service type: " + name);
+    }
+
 }
