05fb9ec207e4f81527a76decf40ee5ccfcc13e4a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-25 19:50:37 +0700

    delete unused class

--- src/main/java/com/bayu/csvfileservice/repository/SInvestInstructionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bayu.csvfileservice.repository;
-
-import com.bayu.csvfileservice.model.SInvestInstruction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SInvestInstructionRepository extends JpaRepository<SInvestInstruction, Long> {
-}
