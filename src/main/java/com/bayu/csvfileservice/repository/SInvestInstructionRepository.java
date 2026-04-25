792c824d0e30909504616a03319d58ef256259df
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-25 19:34:12 +0700

    add repository SInvestInstructionRepository

---
+++ src/main/java/com/bayu/csvfileservice/repository/SInvestInstructionRepository.java
@@ -0,0 +1,7 @@
+package com.bayu.csvfileservice.repository;
+
+import com.bayu.csvfileservice.model.SInvestInstruction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SInvestInstructionRepository extends JpaRepository<SInvestInstruction, Long> {
+}
