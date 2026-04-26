b961108a619a02e3755bd9ed4deab041bedd3019
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-26 18:53:57 +0700

    add method fromDepositTransferBulk

--- src/main/java/com/bayu/csvfileservice/executor/TransferableMapper.java
+++ src/main/java/com/bayu/csvfileservice/executor/TransferableMapper.java
@@ -2,8 +2,12 @@ package com.bayu.csvfileservice.executor;
 
 import com.bayu.csvfileservice.model.DepositTransferMap;
 import com.bayu.csvfileservice.model.ManagementFeeMap;
+import com.bayu.csvfileservice.model.enumerator.ProcessType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 @Slf4j
@@ -41,6 +45,53 @@ public class TransferableMapper {
         dto.setTransferScope(e.getTransferScope());
 
 
+
+        dto.setProcessType(e.getProcessType());
+        dto.setBulkReferenceId(e.getBulkReferenceId());
+        dto.setSiReferenceId(e.getSiReferenceId());
+
         return dto;
     }
+
+    public Transferable fromDepositTransferBulk(List<DepositTransferMap> list) {
+
+        if (list == null || list.isEmpty()) {
+            throw new IllegalArgumentException("Bulk data cannot be empty");
+        }
+
+        DepositTransferMap first = list.get(0);
+
+        BigDecimal totalAmount = BigDecimal.ZERO;
+        StringBuilder siReferenceBuilder = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            DepositTransferMap item = list.get(i);
+
+            totalAmount = totalAmount.add(item.getPrinciple());
+
+            if (i > 0) {
+                siReferenceBuilder.append(", ");
+            }
+
+            siReferenceBuilder.append(item.getSiReferenceId());
+        }
+
+        TransferableAdapter dto = new TransferableAdapter();
+
+        dto.setId(first.getId());
+        dto.setDebitAccount(first.getAccountDebitNo());
+        dto.setCreditAccount(first.getCashAccountNo());
+        dto.setAmount(totalAmount);
+        dto.setDescription("Bulk Deposit Transfer - SI Reference: " + siReferenceBuilder);
+        dto.setTransferMethod(first.getTransferMethod());
+        dto.setTransferScope(first.getTransferScope());
+
+
+        dto.setProcessType(ProcessType.BULK);
+        dto.setBulkReferenceId(first.getBulkReferenceId());
+        dto.setSiReferenceId(siReferenceBuilder.toString());
+
+        return dto;
+    }
+
 }
