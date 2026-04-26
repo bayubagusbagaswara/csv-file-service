90f95dc454d23f8f137199ef8e54a2be5bdca53f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-26 14:51:17 +0700

    add repository DepositTransferMapRepository

---
+++ src/main/java/com/bayu/csvfileservice/repository/DepositTransferMapRepository.java
@@ -0,0 +1,15 @@
+package com.bayu.csvfileservice.repository;
+
+import com.bayu.csvfileservice.model.DepositTransferMap;
+import com.bayu.csvfileservice.model.enumerator.MappingStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DepositTransferMapRepository extends JpaRepository<DepositTransferMap, Long> {
+
+    boolean existsBySiReferenceIdAndMappingStatusIn(
+            String siReferenceId,
+            List<MappingStatus> statuses
+    );
+}
