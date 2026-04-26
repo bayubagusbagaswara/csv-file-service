be848f0dab096ba3cd489d6ecf6ed132ee2db823
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-04-26 14:49:05 +0700

    update entity SInvest

--- src/main/java/com/bayu/csvfileservice/model/SInvest.java
+++ src/main/java/com/bayu/csvfileservice/model/SInvest.java
@@ -18,38 +18,26 @@ import java.time.LocalDate;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SInvestInstruction extends BaseApproval {
+public class SInvest extends BaseApproval {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String imCode;
-
     private String imName;
-
     private String fundCode;
-
     private String fundName;
-
     private String bankCode;
-
     private String bankName;
-
     private String cashAccountName;
-
     private String cashAccountNo;
-
     private String currency;
 
     private BigDecimal principle;
 
     private LocalDate placementDate;
 
-    private String referenceNo;
-
+    @Column(name = "si_reference_id", nullable = false, length = 100)
     private String siReferenceId;
-
-    private boolean processedToPlacementData = false; // true jika sudah proses (sudah masuk ke PlacementData), dan false jika belum masuk ke PlacementData
-
 }
