69acb12a8ecdd2c357cfd8d1bffe548646fba0b9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-14 22:29:48 +0700

    update object DepositTransferMapper

--- src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
@@ -1,6 +1,6 @@
 package com.bayu.csvfileservice.mapper;
 
-import com.bayu.csvfileservice.dto.deposittransfer.SInvestRequest;
+import com.bayu.csvfileservice.dto.sinvest.SInvestRequest;
 import com.bayu.csvfileservice.model.DepositTransferMap;
 import com.bayu.csvfileservice.model.SInvest;
 import org.mapstruct.Mapper;
