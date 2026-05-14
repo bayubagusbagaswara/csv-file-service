890a8861e76196720640a9c93d27cabef64149e0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-14 22:31:44 +0700

    add interface service DepositTransferMapService

---
+++ src/main/java/com/bayu/csvfileservice/service/DepositTransferMapService.java
@@ -0,0 +1,25 @@
+package com.bayu.csvfileservice.service;
+
+import com.bayu.csvfileservice.dto.ProcessResult;
+import com.bayu.csvfileservice.dto.deposittransfer.map.DepositTransferMapDto;
+import com.bayu.csvfileservice.dto.deposittransfer.map.ReleaseDepositTransferHoldRequest;
+import com.bayu.csvfileservice.model.enumerator.ApprovalStatus;
+import com.bayu.csvfileservice.model.enumerator.MappingStatus;
+import com.bayu.csvfileservice.model.enumerator.TransferMethod;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface DepositTransferMapService {
+
+    ProcessResult map(LocalDate currentDate, String userId, String clientIp);
+
+    List<DepositTransferMapDto> getAllByCurrentDate(LocalDate currentDate);
+
+    List<DepositTransferMapDto> getAllByCurrentDateAndMappingStatus(LocalDate currentDate, MappingStatus mappingStatusEnum);
+
+    List<DepositTransferMapDto> getAllByCurrentDateAndApprovalStatusAndTransferMethod(LocalDate currentDate, ApprovalStatus approvalStatusEnum, TransferMethod transferMethodEnum);
+
+    ProcessResult releaseHold(ReleaseDepositTransferHoldRequest request, String releaseBy, String clientIp);
+
+}
