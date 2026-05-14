dc8501b2cbf8564722fdae00dfc1ba937f5caa07
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-14 18:11:02 +0700

    add controller GlobalExceptionHandler

---
+++ src/main/java/com/bayu/csvfileservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.bayu.csvfileservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+}
