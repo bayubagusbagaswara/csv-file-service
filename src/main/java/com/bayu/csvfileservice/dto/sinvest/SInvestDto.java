e1dc46434d69d099ba509686f85b09b96304d419
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-15 19:55:13 +0700

    add some field

--- src/main/java/com/bayu/csvfileservice/dto/sinvest/SInvestDto.java
+++ src/main/java/com/bayu/csvfileservice/dto/sinvest/SInvestDto.java
@@ -1,5 +1,8 @@
 package com.bayu.csvfileservice.dto.sinvest;
 
+import com.bayu.csvfileservice.util.JsonViews;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,5 +10,110 @@ import lombok.Data;
 @Builder
 public class SInvestDto {
 
+    @JsonView({JsonViews.Query.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class})
     private Long id;
+
+    @JsonProperty("imCode")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String imCode;
+
+    @JsonProperty("imName")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String imName;
+
+    @JsonProperty("fundCode")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String fundCode;
+
+    @JsonProperty("fundName")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String fundName;
+
+    @JsonProperty("bankCode")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String bankCode;
+
+    @JsonProperty("bankName")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String bankName;
+
+    @JsonProperty("cashAccountName")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String cashAccountName;
+
+    @JsonProperty("cashAccountNo")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String cashAccountNo;
+
+    @JsonProperty("currency")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String currency;
+
+    @JsonProperty("principle")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String principle;
+
+    @JsonProperty("date")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String date;
+
+    @JsonProperty("referenceNo")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String referenceNo;
+
+    @JsonProperty("siReferenceId")
+    @JsonView({
+            JsonViews.Query.class, JsonViews.Audit.class,
+            JsonViews.AuditForAdd.class, JsonViews.AuditForEdit.class, JsonViews.AuditForDelete.class
+    })
+    private String siReferenceId;
+
+    @JsonView({JsonViews.Query.class})
+    private String transferScope;
+
+    @JsonView({JsonViews.Query.class})
+    private String transferMethod;
+
+    @JsonView({JsonViews.Query.class})
+    private String mappingStatus;
+
+    @JsonView({JsonViews.Query.class})
+    private int rowNumber;
+
 }
