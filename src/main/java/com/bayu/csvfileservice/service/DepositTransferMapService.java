65b264a6cace91ddae668f8110277d28997121cd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-16 21:25:15 +0700

    delete unused method

--- src/main/java/com/bayu/csvfileservice/service/DepositTransferMapService.java
+++ src/main/java/com/bayu/csvfileservice/service/DepositTransferMapService.java
@@ -2,9 +2,7 @@ package com.bayu.csvfileservice.service;
 
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.deposittransfer.map.DepositTransferMapDto;
-import com.bayu.csvfileservice.model.enumerator.ApprovalStatus;
 import com.bayu.csvfileservice.model.enumerator.MappingStatus;
-import com.bayu.csvfileservice.model.enumerator.TransferMethod;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -16,8 +14,6 @@ public interface DepositTransferMapService {
     List<DepositTransferMapDto> getAllByCurrentDate(LocalDate currentDate);
 
     List<DepositTransferMapDto> getAllByCurrentDateAndMappingStatus(LocalDate currentDate, MappingStatus mappingStatusEnum);
-
-    List<DepositTransferMapDto> getAllByCurrentDateAndApprovalStatusAndTransferMethod(LocalDate currentDate, ApprovalStatus approvalStatusEnum, TransferMethod transferMethodEnum);
 
     ProcessResult releaseHold(List<Long> ids, String releaseBy, String clientIp);
 
