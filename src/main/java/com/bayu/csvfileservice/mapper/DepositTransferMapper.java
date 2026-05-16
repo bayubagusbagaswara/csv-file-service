a0c138f1ee1278e6edaa8c802450a8343de5a1a0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-16 21:00:47 +0700

    update method mapper fromSInvestToDepositTransferMap

--- src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
@@ -1,6 +1,5 @@
 package com.bayu.csvfileservice.mapper;
 
-import com.bayu.csvfileservice.dto.sinvest.SInvestRequest;
 import com.bayu.csvfileservice.model.DepositTransferMap;
 import com.bayu.csvfileservice.model.SInvest;
 import org.mapstruct.Mapper;
@@ -8,7 +7,6 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface DepositTransferMapper {
 
-    SInvest toSInvest(SInvestRequest request);
+    DepositTransferMap fromSInvestToDepositTransferMap(SInvest sInvest);
 
-    DepositTransferMap toDepositTransferMap(SInvest sInvest);
 }
