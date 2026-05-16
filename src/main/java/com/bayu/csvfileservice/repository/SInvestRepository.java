fe50e2db4fb66a1462c55a26cec0e0cf0f393aad
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-16 09:51:39 +0700

    add method existsBySiReferenceIdAndDate

--- src/main/java/com/bayu/csvfileservice/repository/SInvestRepository.java
+++ src/main/java/com/bayu/csvfileservice/repository/SInvestRepository.java
@@ -4,7 +4,12 @@ package com.bayu.csvfileservice.repository;
 import com.bayu.csvfileservice.model.SInvest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface SInvestRepository extends JpaRepository<SInvest, Long> {
 
     boolean existsBySiReferenceId(String siReferenceId);
+
+    boolean existsBySiReferenceIdAndDate(String siReferenceId, LocalDate date);
+
 }
