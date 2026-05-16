274cb990c249ce24de620eb5ea28d7da817c13d6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-16 21:02:11 +0700

    update field ids

--- src/main/java/com/bayu/csvfileservice/dto/deposittransfer/map/ReleaseDepositTransferHoldRequest.java
+++ src/main/java/com/bayu/csvfileservice/dto/deposittransfer/map/ReleaseDepositTransferHoldRequest.java
@@ -3,10 +3,13 @@ package com.bayu.csvfileservice.dto.deposittransfer.map;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ReleaseDepositTransferHoldRequest {
 
-    private Long id;
+    private List<Long> ids;
     private String releaseBy;
+
 }
