dac8b9833d358d58bd518e1fbf16e9a6cdfc4ae6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-16 21:24:25 +0700

    add method fromEntityToDto and fromEntitiesToDtos

--- src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
+++ src/main/java/com/bayu/csvfileservice/mapper/DepositTransferMapper.java
@@ -1,12 +1,19 @@
 package com.bayu.csvfileservice.mapper;
 
+import com.bayu.csvfileservice.dto.deposittransfer.map.DepositTransferMapDto;
 import com.bayu.csvfileservice.model.DepositTransferMap;
 import com.bayu.csvfileservice.model.SInvest;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface DepositTransferMapper {
 
     DepositTransferMap fromSInvestToDepositTransferMap(SInvest sInvest);
+
+    DepositTransferMapDto fromEntityToDto(DepositTransferMap entity);
+
+    List<DepositTransferMapDto> fromEntitiesToDtos(List<DepositTransferMap> entities);
 
 }
