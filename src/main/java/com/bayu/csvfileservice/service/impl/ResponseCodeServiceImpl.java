f2238ff1d30174f279eb7de0786d2e90041b3bae
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 10:55:29 +0700

    implement method getSuccessCodes and isSuccess

--- src/main/java/com/bayu/csvfileservice/service/impl/ResponseCodeServiceImpl.java
+++ src/main/java/com/bayu/csvfileservice/service/impl/ResponseCodeServiceImpl.java
@@ -14,8 +14,10 @@ public class ResponseCodeServiceImpl implements ResponseCodeService {
     private final ResponseCodeRepository responseCodeRepository;
 
     public static final String INSUFFICIENT_BALANCE = "INSUFFICIENT_BALANCE";
+    public static final String SUCCESS = "SUCCESS";
 
     private List<String> insufficientBalanceCodes;
+    private List<String> successCodes;
 
     @Override
     public List<String> getInsufficientBalanceCodes() {
@@ -26,9 +28,23 @@ public class ResponseCodeServiceImpl implements ResponseCodeService {
     }
 
     @Override
+    public List<String> getSuccessCodes() {
+        if (successCodes == null) {
+            successCodes = responseCodeRepository.findCodesByName(SUCCESS);
+        }
+        return successCodes;
+    }
+
+    @Override
     public boolean isInsufficientBalance(String code) {
         if (code == null) return false;
         return getInsufficientBalanceCodes().contains(code);
+    }
+
+    @Override
+    public boolean isSuccess(String code) {
+        if (code == null) return false;
+        return getSuccessCodes().contains(code);
     }
 
 }
