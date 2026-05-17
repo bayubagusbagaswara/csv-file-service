678f3b39398d76a27c761aa6ca7b0b0292af5598
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 16:03:58 +0700

    delete unused method

--- src/main/java/com/bayu/csvfileservice/service/ManagementFeeMapService.java
+++ src/main/java/com/bayu/csvfileservice/service/ManagementFeeMapService.java
@@ -1,17 +1,10 @@
 package com.bayu.csvfileservice.service;
 
 import com.bayu.csvfileservice.dto.ProcessResult;
-import com.bayu.csvfileservice.dto.transaction.CreateSingleTransactionRequest;
 import com.bayu.csvfileservice.model.enumerator.Month;
-
-import java.util.List;
 
 public interface ManagementFeeMapService {
 
     ProcessResult map(Month monthEnum, Integer year, String clientIp);
-
-    ProcessResult create(List<CreateSingleTransactionRequest> requests, String clientIp);
-
-    ProcessResult send(List<Long> ids, String userId, String clientIp);
 
 }
