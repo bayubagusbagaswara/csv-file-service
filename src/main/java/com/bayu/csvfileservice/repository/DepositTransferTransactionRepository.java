41923b9d66e49abe5b020ef78ed95c87d8880f2e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 10:54:21 +0700

    add method query findAllByDate and findAllByProcessType

--- src/main/java/com/bayu/csvfileservice/repository/DepositTransferTransactionRepository.java
+++ src/main/java/com/bayu/csvfileservice/repository/DepositTransferTransactionRepository.java
@@ -1,7 +1,16 @@
 package com.bayu.csvfileservice.repository;
 
 import com.bayu.csvfileservice.model.DepositTransferTransaction;
+import com.bayu.csvfileservice.model.enumerator.ProcessType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DepositTransferTransactionRepository extends JpaRepository<DepositTransferTransaction, Long> {
+
+    List<DepositTransferTransaction> findAllByDate(LocalDate currentDate);
+
+    List<DepositTransferTransaction> findAllByProcessTypeAndDate(ProcessType processType, LocalDate currentDate);
+
 }
