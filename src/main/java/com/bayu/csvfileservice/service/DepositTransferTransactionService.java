a9e8f1a39bd8f506c88ad9ea914aeee133990f79
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 10:54:30 +0700

    update all method service

--- src/main/java/com/bayu/csvfileservice/service/DepositTransferTransactionService.java
+++ src/main/java/com/bayu/csvfileservice/service/DepositTransferTransactionService.java
@@ -2,20 +2,24 @@ package com.bayu.csvfileservice.service;
 
 import com.bayu.csvfileservice.dto.ProcessResult;
 import com.bayu.csvfileservice.dto.deposittransfer.transaction.*;
+import com.bayu.csvfileservice.model.enumerator.ProcessType;
+import com.bayu.csvfileservice.model.enumerator.TransferMethod;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface DepositTransferTransactionService {
 
-    ProcessResult createSingleTransaction(CreateSingleDepositTransferTransactionRequest request, String userId, String clientIp);
+    ProcessResult createSingle(Long id, TransferMethod transferMethod, String userId, String clientIp);
 
-    ProcessResult createBulkTransaction(CreateBulkDepositTransferTransactionRequest request, String userId, String clientIp);
+    ProcessResult createBulk(List<Long> ids, TransferMethod transferMethod, String userId, String clientIp);
 
-    ProcessResult sendTransaction(SendDepositTransferTransactionRequest request, String userId, String clientIp);
+    ProcessResult send(List<Long> ids, String userId, String clientIp);
 
-    ProcessResult rejectTransaction(RejectDepositTransferTransactionRequest request, String userId, String clientIp);
+    ProcessResult reject(List<Long> ids, String userId, String clientIp);
 
-    List<DepositTransferTransactionDto> getAllTransactionByCurrentDate(LocalDate currentDate);
+    List<DepositTransferTransactionDto> getAllByCurrentDate(LocalDate currentDate);
+
+    List<DepositTransferTransactionDto> getAllByProcessTypeAndCurrentDate(ProcessType processType, LocalDate currentDate);
 
 }
