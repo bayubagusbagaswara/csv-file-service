437a2f92b12333a518144ad11e6225cd88bfaa6e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 19:33:37 +0700

    add annotation

--- src/main/java/com/bayu/csvfileservice/dto/CreateNcbsResponse.java
+++ src/main/java/com/bayu/csvfileservice/dto/CreateNcbsResponse.java
@@ -1,14 +1,18 @@
 package com.bayu.csvfileservice.dto;
 
 import com.bayu.csvfileservice.model.enumerator.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateNcbsResponse {
 
     private LocalDateTime createdDate;
