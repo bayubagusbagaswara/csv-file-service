728b5c8f7102aef6e2ab0f4344f2b04c6b737e02
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 19:36:12 +0700

    change return data TransferExecutionResult

--- src/main/java/com/bayu/csvfileservice/executor/TransferExecutor.java
+++ src/main/java/com/bayu/csvfileservice/executor/TransferExecutor.java
@@ -1,12 +1,11 @@
 package com.bayu.csvfileservice.executor;
 
-import com.bayu.csvfileservice.model.NcbsResponse;
 import com.bayu.csvfileservice.model.enumerator.TransferMethod;
 
 public interface TransferExecutor {
 
     boolean supports(TransferMethod transferMethod);
 
-    NcbsResponse execute(Transferable item);
+    TransferExecutionResult execute(Transferable item);
 
 }
