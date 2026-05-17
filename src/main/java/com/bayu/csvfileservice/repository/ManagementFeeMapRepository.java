f4538be06e9c59c9415990b030dd8386f7c1860c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 16:32:46 +0700

    add some method query

--- src/main/java/com/bayu/csvfileservice/repository/ManagementFeeMapRepository.java
+++ src/main/java/com/bayu/csvfileservice/repository/ManagementFeeMapRepository.java
@@ -5,6 +5,8 @@ import com.bayu.csvfileservice.model.enumerator.MappingStatus;
 import com.bayu.csvfileservice.model.enumerator.Month;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ManagementFeeMapRepository extends JpaRepository<ManagementFeeMap, Long> {
 
     void deleteByMonthAndYear(Month month, Integer year);
@@ -12,5 +14,13 @@ public interface ManagementFeeMapRepository extends JpaRepository<ManagementFeeM
     void deleteByMonthAndYearAndStatus(Month month, Integer year, MappingStatus status);
 
     boolean existsByReferenceCombination(String referenceCombination);
+
+    void deleteByMonthAndYearAndStatusIn(Month month, Integer year, List<MappingStatus> mappingStatuses);
+
+    boolean existsByMonthAndYearAndStatusIn(Month month, Integer year, List<MappingStatus> list);
+
+    boolean existsByReferenceCombinationAndStatusIn(String referenceCombination, List<MappingStatus> list);
+
+    void deleteByReferenceCombinationAndStatus(String referenceCombination, MappingStatus mappingStatus);
 
 }
