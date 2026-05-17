c4b7c95bcd220b52bf9c9f081ec6d687a341d72b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-17 16:45:36 +0700

    update all method service

--- src/main/java/com/bayu/csvfileservice/service/ManagementFeeTransactionService.java
+++ src/main/java/com/bayu/csvfileservice/service/ManagementFeeTransactionService.java
@@ -1,16 +1,16 @@
 package com.bayu.csvfileservice.service;
 
 import com.bayu.csvfileservice.dto.ProcessResult;
-import com.bayu.csvfileservice.dto.transaction.CreateSingleTransactionRequest;
+import com.bayu.csvfileservice.model.enumerator.TransferMethod;
 
 import java.util.List;
 
 public interface ManagementFeeTransactionService {
 
-    ProcessResult create(List<CreateSingleTransactionRequest> requests, String clientIp);
+    ProcessResult create(Long id, TransferMethod transferMethod, String description, String userId, String clientIp);
 
     ProcessResult send(List<Long> ids, String userId, String clientIp);
 
+    ProcessResult reject(List<Long> ids, String userId, String clientIp);
 
-    // reject transaction
 }
