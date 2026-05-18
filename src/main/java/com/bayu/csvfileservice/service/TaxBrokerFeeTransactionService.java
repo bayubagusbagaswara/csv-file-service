e12c98eb122f58077c065af5bc06acad4410b7d9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2026-05-19 06:59:38 +0700

    add service TaxBrokerFeeTransactionService

---
+++ src/main/java/com/bayu/csvfileservice/service/TaxBrokerFeeTransactionService.java
@@ -0,0 +1,16 @@
+package com.bayu.csvfileservice.service;
+
+import com.bayu.csvfileservice.dto.ProcessResult;
+import com.bayu.csvfileservice.model.enumerator.TransferMethod;
+
+import java.util.List;
+
+public interface TaxBrokerFeeTransactionService {
+
+    ProcessResult create(Long id, TransferMethod transferMethod, String description, String userId, String clientIp);
+
+    ProcessResult send(List<Long> ids, String userId, String clientIp);
+
+    ProcessResult reject(List<Long> ids, String userId, String clientIp);
+
+}
